a671edfa7d4fb9d6f79e90394042f4c19902e0a4
Loïc Prenevost <loicprenevost@Loics-MacBook-Pro.local>
2019-05-31 11:06:54 -0400

    Comments

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ var signature = '&api_key=' + API_KEY + '&format=json';
 
 // GENRES
 var MAX_GENRES = 75;
-var MAX_PER_GENRE = 10;
+var MAX_PER_GENRE = 1000;
 var genres = [];
-var dataset = [];
 
+/**
+ * @getSongsPerGenre
+ * Recursive function that calls the lastFM API to get an array of songs
+ * based on a given genre. The function calls itself until the number
+ * of songs retrieved is equal to the MAX_PER_GENRE constant
+ *
+ * @param {string} genre the genre of the songs to retrieve 
+ * @param {number} page page parameter of lastFM API
+ * @param {Array} songs the array containing the retrieved songs
+ */
 const getSongsPerGenre = async (genre, page, songs) => {
   // Set page to 1 if not defined
   page = (page === undefined) ? 1 : page;
@@ -21,8 +30,8 @@ const getSongsPerGenre = async (genre, page, songs) => {
 
   return axios.get(baseURL + query + signature).then(response => {
     if (response.data.tracks.track) {
-      response.data.tracks.track.forEach(function(track) {
-        if (songs.length < MAX_PER_GENRE) { // TODO: check if song already in songs
+      response.data.tracks.track.forEach(function (track) {
+        if (songs.length < MAX_PER_GENRE) { // TODO: check if song already retrieved (in another genre also)
           var song = {
             title: track.name,
             artist: track.artist.name,
@@ -31,7 +40,7 @@ const getSongsPerGenre = async (genre, page, songs) => {
           songs.push(song);
         }
       });
-      
+
       // Check if we have less than the maximum
       if (songs.length < MAX_PER_GENRE) {
         // Recursive call
@@ -41,35 +50,51 @@ const getSongsPerGenre = async (genre, page, songs) => {
         return songs;
       }
     }
-  }) .catch(error => {
+  }).catch(error => {
     console.log(error);
     return false;
   });
 };
 
+/**
+ * @getSongs
+ * Loops an array of genres
+ * Foreach genre, calls the getSongsPerGenre method which 
+ * returns an array of songs
+ */
 const getSongs = async () => {
-  genres.forEach(async function(genre) {
+  genres.forEach(async function (genre) {
     var result = await getSongsPerGenre(genre, 1, []);
     console.log(result.length + ' songs for genre ' + genre);
     console.log('-------');
   });
 };
 
+/**
+ * @getGenres
+ * Calls the lastFM API to get the top genres
+ * Uses constant MAX_GENRES to specify how many genres
+ * must be returned 
+ */
 const getGenres = async () => {
   // Construct query
   var query = '?method=tag.getTopTags&num_res=' + MAX_GENRES;
   return axios.get(baseURL + query + signature).then(response => {
     if (response.data.toptags.tag) {
-      response.data.toptags.tag.forEach(function(genre) {
+      response.data.toptags.tag.forEach(function (genre) {
         genres.push(genre.name);
       });
     }
-  }) .catch(error => {
+  }).catch(error => {
     console.log(error);
     return false;
   });
 };
 
+/**
+ * @crawl
+ * Starts the programm
+ */
 const crawl = async () => {
   console.log('');
   console.log('---------- Getting genres ----------');
