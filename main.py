f8971ccf5323776eaa0d6c17e14612771e247ddc
loiCQAM <loicprenevost@gmail.com>
2019-06-14 10:58:39 -0400

    primitive extraction

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ elif (not sys.argv[1].isdigit() or not sys.argv[2].isdigit()):
 else:
     print("Step 1 --- lastFM extraction")
 
+    lastFM_songs = []
     songs = []
     max_genres = int(sys.argv[1])
     songs_per_genre = int(sys.argv[2])
@@ -24,15 +25,23 @@ else:
     # get songs
     for genre in genres:
         extraction = lastFM.get_songs_per_genre(genre, [], 1, songs_per_genre)
-        songs = songs + extraction
+        lastFM_songs = lastFM_songs + extraction
 
-    print(str(len(songs)) + " songs retrieved")
+    print(str(len(lastFM_songs)) + " songs retrieved")
 
     print("")
     print("Step 2 --- Spotify extraction")
-    for song in songs:
+    for song in lastFM_songs:
         s = spotifyAPI.search_song(song['title'], song['artist'])
-        print(s)
+        if (s is False):
+            print("Error retrieving " +
+                  song['title'] + " from " + song['artist'])
+        else:
+            songs.append(s)
+
+    print(str(len(songs)) + " songs retrieved")
+    if (len(songs) < len(lastFM_songs)):
+        print(str(len(lastFM_songs) - len(songs)) + " songs lost in conversion")
 
     print("")
     print("DONE !")
--- spotifyAPI.py
+++ spotifyAPI.py
@@ -13,6 +13,8 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def search_song(title, artist):
     song = {}
+
+    # basic song info from API
     result = sp.search(q=title + ', ' + artist, type='track', limit=1)
     if (result['tracks']['items'] and len(result['tracks']['items']) > 0):
         s = result['tracks']['items'][0]
@@ -20,8 +22,24 @@ def search_song(title, artist):
         song['title'] = title
         song['artist'] = artist
         song['primitives'] = {}
-        song['primitives']['popularity'] = s['popularity']
-        song['primitives']['duration'] = s['duration_ms']
+
+        # get primitives from API
+        features = sp.audio_features([song['id']])
+        if (features and features[0] is not None):
+            song['primitives']['duration'] = features[0]['duration_ms']
+            song['primitives']['energy'] = features[0]['energy']
+            song['primitives']['liveness'] = features[0]['liveness']
+            song['primitives']['tempo'] = features[0]['tempo']
+            song['primitives']['speechiness'] = features[0]['speechiness']
+            song['primitives']['acousticness'] = features[0]['acousticness']
+            song['primitives']['instrumentalness'] = features[0]['instrumentalness']
+            song['primitives']['time_signature'] = features[0]['time_signature']
+            song['primitives']['danceability'] = features[0]['danceability']
+            song['primitives']['key'] = features[0]['key']
+            song['primitives']['loudness'] = features[0]['loudness']
+            song['primitives']['valence'] = features[0]['valence']
+        else:
+            return False
     else:
         return False
 
